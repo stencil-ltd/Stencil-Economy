9d0dc39b3768777d56f7b70f4b64b6556bc38708
Aaron Sarazan <aaron@sarazan.net>
2018-11-11 11:59:05 -0800

    everything went better than expected

--- Scripts/Merch/Data/MerchGroup.cs
+++ Scripts/Merch/Data/MerchGroup.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Common;
 using Malee;
+using Store;
 using UnityEngine;
 using Util;
 
@@ -9,9 +11,13 @@ namespace Merch.Data
     [CreateAssetMenu(menuName = "Economy/Merch Group")]
     public class MerchGroup : StencilData
     {
+        private static Dictionary<CarStoreStates.State, MerchGroup> _stateMap =
+            new Dictionary<CarStoreStates.State, MerchGroup>();
+
         [Header("Settings")]
         public bool SingleEquip = true;
         public bool LockItems = true;
+        public CarStoreStates.State UiState;
         
         [Reorderable]
         public GrantArray Grants;
@@ -23,6 +29,14 @@ namespace Merch.Data
         public void NotifyChanged()
         {
             OnChange?.Invoke();
+        }
+
+        public static MerchGroup ForState(CarStoreStates.State state)
+            => _stateMap[state];
+
+        private void OnEnable()
+        {
+            _stateMap[UiState] = this;
         }
     }
 }
