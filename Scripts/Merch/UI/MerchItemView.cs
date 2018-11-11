c633948522dd643bc37873519b84563d819ce84f
Aaron Sarazan <aaron@sarazan.net>
2018-11-10 21:12:06 -0800

    click listeners

--- Scripts/Merch/UI/MerchItemView.cs
+++ Scripts/Merch/UI/MerchItemView.cs
@@ -1,11 +1,24 @@
 using Merch.System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Merch.UI
 {
     public abstract class MerchItemView : MonoBehaviour
     {
         public MerchResult Result { get; private set; }
+        
+        private void Awake()
+        {
+            GetComponent<Button>()?.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            if (Result.State.Selected)
+                MerchSystem.Instance.SetEquipped(Result.Item, true);
+            else MerchSystem.Instance.SetSelected(Result.Item);
+        }
         
         public void SetResult(MerchResult result)
         {
