f78e4fd45d2686aa9a32a04af2e25f00a8cad75d
Aaron Sarazan <aaron@sarazan.net>
2018-11-12 17:21:52 -0800

    setting up purchase screen

--- Scripts/Currencies/Price.cs
+++ Scripts/Currencies/Price.cs
@@ -9,6 +9,12 @@ namespace Currencies
         [Tooltip("Null if IAP.")]
         public Currency Currency;
         public int Amount;
+
+        public bool CanAfford 
+            => Currency.CanSpend(Amount);
+
+        public CurrencyOperation Purchase()
+            => Currency.Spend(Amount);
         
         public static implicit operator Currency(Price price)
             => price.Currency;
